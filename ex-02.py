950deeedcf106992ac508999973b97484e9fc1d1
jotaalves10 <joaopedroalves2007.jp@gmail.com>
2025-03-24 19:00:56 -0300

    Create ex-02.py

---
+++ ex-02.py
@@ -0,0 +1,5 @@
+#2. Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+
+print("João pedro alves ramos")
+print("Rio de janeiro")
+print("Belford roxo")
