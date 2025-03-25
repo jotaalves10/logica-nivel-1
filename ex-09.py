0a950877adc52fedcf1a372ef012dbc3f34402b0
jotaalves10 <joaopedroalves2007.jp@gmail.com>
2025-03-24 21:42:20 -0300

    Create ex-09.py

---
+++ ex-09.py
@@ -0,0 +1,9 @@
+#9 - escreva um software que some dois numeros.
+
+
+print("Qual o primeiro valor?")
+numero1=int(input())
+print("Qual o segundo valor?")
+numero2=int(input())
+resultado=numero1+numero2
+print(resultado)
