046db222350c49ffd9ef345d35b8d52a4265002a
jotaalves10 <joaopedroalves2007.jp@gmail.com>
2025-05-01 19:46:32 -0300

    Create ex-15.py

---
+++ ex-15.py
@@ -0,0 +1,7 @@
+#15) Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo).
+
+valor=int(input("Digite um valor:"))
+if valor>=0:
+  print("Ele é positivo")
+else:
+    print("Ele é negativo")
